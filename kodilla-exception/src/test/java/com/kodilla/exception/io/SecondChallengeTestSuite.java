d5f4541aa45677cca0ad65c6428e7a1af9c156ea
Michal <mrys@tgo.pl>
2022-07-20 13:08:07 +0200

    8.3 Second Challenge

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/SecondChallengeTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.io;
+
+import com.kodilla.exception.test.SecondChallenge;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SecondChallengeTestSuite {
+
+    @Test
+    public void testProbablyWillThrowException() {
+        // given
+        SecondChallenge secondChallenge = new SecondChallenge();
+        // when & then
+        assertAll(
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2.0, 1.4)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1, 1.5)),
+                () -> assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1, 1.6)),
+                () -> assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1.99, 1.4))
+        );
+    }
+}
