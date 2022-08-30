2c8a50688043ae037fc29df4a4e2bb2ffebd3c4c
Michal <mrys@tgo.pl>
2022-08-30 18:43:44 +0200

    14.2 Calculator - Spring

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,2 +1,38 @@
-package com.kodilla.spring.calculator;public class Calculator {
+package com.kodilla.spring.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+
+    private final Display display;
+
+    public Calculator(Display display) {
+        this.display = display;
+    }
+
+    public double add(double a, double b) {
+
+        display.displayValue(a+b);
+        return a+b;
+    }
+
+    public double sub(double a, double b) {
+
+        display.displayValue(a-b);
+        return a-b;
+    }
+
+    public double mul(double a, double b) {
+
+        display.displayValue(a*b);
+        return a*b;
+    }
+
+    public double div(double a, double b) {
+
+        display.displayValue(a/b);
+        return a/b;
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,2 +1,34 @@
-package com.kodilla.spring.calculator;public class CalculatorTestSuite {
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class CalculatorTestSuite {
+
+
+
+    @Test
+    void CalculatorTestSuite() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+
+
+        //When
+        double add = calculator.add(5, 4);
+        double sub = calculator.sub(6.2, 5.1);
+        double mul = calculator.mul(3.3, 4.1);
+        double div = calculator.div(6.4, 2);
+
+        //Then
+        assertEquals(9, add);
+        assertEquals(1.1, sub, 0.01);
+        assertEquals(13.53, mul, 0.01);
+        assertEquals(3.2, div);
+    }
 }
